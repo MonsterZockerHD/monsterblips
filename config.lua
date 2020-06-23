b8e1401f4d9c31e47c7589a6410bb21753612b66
Lennox <lennox23.pathfinder3@gmail.com>
2020-06-23 23:51:15 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,14 +2,8 @@
 
 Config = {}
 
-Config.CircleBlips = {
-	-- config name, (coords) x,y,z, blip name, blip color, blip sprite, blip radius
+Config.Blips = {
+	-- config name, (coords) x,y,z, blip name, blip color, blip sprite, blip radius (close after the sprite to use no radius)
 	FreeCandy = {coords = vector3(-1757.96, 3079.44, 32.82), name = 'Free candy', color = 1, sprite = 121, radius = 75.0},
 	MoreFreeCandy = {coords = vector3(1088.8, -1993.1, 30.97), name = ' More free candy', color = 1, sprite = 121, radius = 100.0},
-}
-
-Config.Blips = {
-	-- config name, (coords) x,y,z, blip name, blip color, blip sprite
-	Burgers = {coords = vector3(-1757.96, 3079.44, 32.82), name = 'Burgers', color = 1, sprite = 365,
-	Fries = {coords = vector3(1088.8, -1993.1, 30.97), name = 'Fries', color = 1, sprite = 365},
 }
