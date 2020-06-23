bdb5011569002803a4a3416e1974cf4798b548a4
Lennox <lennox23.pathfinder3@gmail.com>
2020-06-23 23:54:29 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 
 author 'MonsterZockerHD' --Basically I don't care about this
 description 'A resource enabling the possibility to create blips with/without radius'
-version '1.0.0'
+version '1.1.0'
 
 client_scripts {
     'client/main.lua',
