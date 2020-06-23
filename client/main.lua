1aaeb561ced73ce15cbcfbd75ef8a849b4818085
Lennox <lennox23.pathfinder3@gmail.com>
2020-06-23 23:51:55 +0200

    fixed logic

--- client/main.lua
+++ client/main.lua
@@ -1,4 +1,4 @@
-function BlipWithCircle(coords, text, radius, color, sprite)
+function Blip(coords, text, radius, color, sprite)
 	local blip = AddBlipForRadius(coords, radius)
 
 	SetBlipHighDetail(blip, true)
@@ -16,29 +16,8 @@ function BlipWithCircle(coords, text, radius, color, sprite)
 	EndTextCommandSetBlipName(blip)
 end
 
-function Blip(coords, text, color, sprite)
-	local blip = AddBlipForRadius(coords)
-
-	SetBlipHighDetail(blip, true)
-	SetBlipColour(blip, 1)
-	SetBlipAlpha (blip, 128)
-	blip = AddBlipForCoord(coords)
-	SetBlipHighDetail(blip, true)
-	SetBlipSprite (blip, sprite)
-	SetBlipScale  (blip, 1.0)
-	SetBlipColour (blip, color)
-	SetBlipAsShortRange(blip, true)
-
-	BeginTextCommandSetBlipName("STRING")
-	AddTextComponentString(text)
-	EndTextCommandSetBlipName(blip)
-end
-
 Citizen.CreateThread(function()
-	for k,zone in pairs(Config.CircleBlips) do
-		BlipWithCircle(zone.coords, zone.name, zone.radius, zone.color, zone.sprite)
-	end
 	for k,zone in pairs(Config.Blips) do
-		BlipWithCircle(zone.coords, zone.name, zone.radius, zone.color, zone.sprite)
+		Blip(zone.coords, zone.name, zone.radius, zone.color, zone.sprite)
 	end
 end)
