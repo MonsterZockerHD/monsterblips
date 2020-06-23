a8528ec3016c9805c6c3bfe2aa96ab2fc64e3702
Lennox <lennox23.pathfinder3@gmail.com>
2020-06-23 23:53:16 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,8 +2,10 @@
 
 Config = {}
 
+-- config name, (coords) x,y,z, blip name, blip color, blip sprite, blip radius
+-- if you don't want a radius just close } after setting the sprite
 Config.Blips = {
-	-- config name, (coords) x,y,z, blip name, blip color, blip sprite, blip radius (close after the sprite to use no radius)
 	FreeCandy = {coords = vector3(-1757.96, 3079.44, 32.82), name = 'Free candy', color = 1, sprite = 121, radius = 75.0},
 	MoreFreeCandy = {coords = vector3(1088.8, -1993.1, 30.97), name = ' More free candy', color = 1, sprite = 121, radius = 100.0},
+	
 }
